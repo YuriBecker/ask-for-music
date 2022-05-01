f7f312602c1d613fd5519329b22b29cc007ad42d
Yuri Becker <becker.yr@gmail.com>
2022-05-01 12:18:57 -0300

    feat: update favicon icon

--- pages/_document.js
+++ pages/_document.js
@@ -11,7 +11,7 @@ export default class _Document extends Document {
       <Html lang="pt-BR">
         <Head>
           <link rel="manifest" href="/manifest.json" />
-          <link rel="icon" href="/favicon.ico" type="image/svg+xml" />
+          <link rel="icon" href="/favicon.ico" type="image/x-icon" />
 
           <link
             rel="apple-touch-icon"
